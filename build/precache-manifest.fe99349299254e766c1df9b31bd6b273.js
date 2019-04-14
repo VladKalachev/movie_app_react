64b9e7afb2d7daa14403b6d90df80c5067c6234e
Corvax <vlvete1990@yandex.ru>
2019-04-15 00:18:57 +0300

    v0.0.2

---
+++ build/precache-manifest.fe99349299254e766c1df9b31bd6b273.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7fa1ce463d8b6e93014e",
+    "url": "/static/js/main.7fa1ce46.chunk.js"
+  },
+  {
+    "revision": "4ebef35c9e6b8daaf3a0",
+    "url": "/static/js/1.4ebef35c.chunk.js"
+  },
+  {
+    "revision": "7fa1ce463d8b6e93014e",
+    "url": "/static/css/main.3b34fdee.chunk.css"
+  },
+  {
+    "revision": "163f4c919b61d497ef3b6c123c5e28ca",
+    "url": "/index.html"
+  }
+];
